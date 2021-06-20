65baf1fc5198dc7d79d21320dcc6be2c700460a2
Zhenboro <zhenboro@outlook.com>
2021-06-20 02:11:48 -0300

    Origen del todo

---
+++ FileBoxCracker/Main.designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(296, 339)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(78, 23)
+        Me.Button3.TabIndex = 93
+        Me.Button3.Text = "< Descifrar <"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(380, 339)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(275, 161)
+        Me.RichTextBox2.TabIndex = 92
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(15, 339)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(275, 161)
+        Me.RichTextBox1.TabIndex = 91
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(226, 197)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox5.TabIndex = 90
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(132, 200)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(55, 13)
+        Me.Label6.TabIndex = 87
+        Me.Label6.Text = "CryptoKey"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(287, 283)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 35)
+        Me.Button2.TabIndex = 86
+        Me.Button2.Text = "Save It!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(226, 171)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox4.TabIndex = 85
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(132, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 84
+        Me.Label4.Text = "IsRegistered"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(226, 145)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox3.TabIndex = 83
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(132, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(32, 13)
+        Me.Label3.TabIndex = 82
+        Me.Label3.Text = "Email"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(226, 119)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox2.TabIndex = 81
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(132, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 80
+        Me.Label2.Text = "Password"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(226, 93)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(312, 20)
+        Me.TextBox1.TabIndex = 79
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(132, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 78
+        Me.Label1.Text = "UserName"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(172, 58)
+        Me.Button1.TabIndex = 77
+        Me.Button1.Text = "Load It!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(671, 516)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Main"
+        Me.Text = "FileBox Cracker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button3 As Button
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ FileBoxCracker/Main.vb
@@ -0,0 +1,121 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Imports Microsoft.VisualBasic.CompilerServices
+Public Class Main
+    Dim DirectorioRaiz As String = "C:\Users\" + Environment.UserName + "\FileBoxData"
+    Dim ArchivoDB As String = DirectorioRaiz & "\DataBase_FileBox.db"
+    Public Shared tripleDESCryptoServiceProvider_0 As TripleDESCryptoServiceProvider = New TripleDESCryptoServiceProvider()
+    Public Shared md5CryptoServiceProvider_0 As MD5CryptoServiceProvider = New MD5CryptoServiceProvider()
+    Dim Llave As String = "Pw3jakfBIVYydqFZhjyQLwaJTWGPn9"
+
+    Dim UserName As String
+    Dim Password As String
+    Dim Email As String
+    Dim IsRegistered As String
+    Dim CryptoKey As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoadDB()
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        UserName = TextBox1.Text
+        Password = TextBox2.Text
+        Email = TextBox3.Text
+        IsRegistered = TextBox4.Text
+        CryptoKey = TextBox5.Text
+        SaveDB()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        RichTextBox1.AppendText(vbCrLf & Desencriptar(RichTextBox2.Text))
+    End Sub
+
+    Public Sub LoadDB()
+        Try
+            Dim RAWContent As String = My.Computer.FileSystem.ReadAllText(ArchivoDB)
+            Dim DecryptContent As String = Desencriptar(RAWContent)
+            Dim lines As String() = New TextBox() With {
+            .Text = DecryptContent
+            }.Lines
+            RichTextBox1.AppendText(vbCrLf & "<--- DB Raw Content --->")
+            RichTextBox1.AppendText(vbCrLf & RAWContent)
+            RichTextBox1.AppendText(vbCrLf & "<--- DB Decrypted Content Loaded --->")
+            RichTextBox1.AppendText(vbCrLf & DecryptContent)
+            Me.UserName = lines(0).Split(New Char() {">"c})(1).Trim()
+            Me.Password = lines(1).Split(New Char() {">"c})(1).Trim()
+            Me.Email = lines(2).Split(New Char() {">"c})(1).Trim()
+            Me.IsRegistered = lines(3).Split(New Char() {">"c})(1).Trim()
+            Me.CryptoKey = lines(4).Split(New Char() {">"c})(1).Trim()
+
+            TextBox1.Text = UserName
+            TextBox2.Text = Password
+            TextBox3.Text = Email
+            TextBox4.Text = IsRegistered
+            TextBox5.Text = CryptoKey
+            Console.WriteLine("USER DB:" &
+                              vbCrLf &
+                              "    " & UserName & vbCrLf &
+                              "    " & Password & vbCrLf &
+                              "    " & Email & vbCrLf &
+                              "    " & IsRegistered & vbCrLf &
+                              "    " & CryptoKey)
+            RichTextBox1.ScrollToCaret()
+        Catch ex As Exception
+            Console.WriteLine("[Debugger@GetData]Error: " + ex.Message)
+        End Try
+    End Sub
+
+    Sub SaveDB()
+        Try
+            If My.Computer.FileSystem.FileExists(ArchivoDB) = True Then
+                My.Computer.FileSystem.DeleteFile(ArchivoDB)
+            End If
+            Dim FormatoDB As String = "Username>" & Me.UserName &
+                            vbCrLf & "Password>" & Me.Password &
+                            vbCrLf & "Email>" & Me.Email &
+                            vbCrLf & "IsRegistered>" & Me.IsRegistered &
+                            vbCrLf & "CryptoKey>" & Me.CryptoKey
+            Dim EncryptedContent As String = Encriptar(FormatoDB)
+            RichTextBox1.AppendText(vbCrLf & "<--- DB Raw Content --->")
+            RichTextBox1.AppendText(vbCrLf & FormatoDB)
+            RichTextBox1.AppendText(vbCrLf & "<--- DB Encrypted Content Saved --->")
+            RichTextBox1.AppendText(vbCrLf & EncryptedContent)
+            My.Computer.FileSystem.WriteAllText(Me.ArchivoDB, EncryptedContent, False)
+            RichTextBox1.ScrollToCaret()
+            LoadDB()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Public Function Encriptar(string_1 As String) As String
+        Dim result As String
+        If Operators.CompareString(Strings.Trim(string_1), "", False) <> 0 Then
+            tripleDESCryptoServiceProvider_0.Key = md5CryptoServiceProvider_0.ComputeHash(New UnicodeEncoding().GetBytes(Me.Llave))
+            tripleDESCryptoServiceProvider_0.Mode = CipherMode.ECB
+            Dim cryptoTransform As ICryptoTransform = tripleDESCryptoServiceProvider_0.CreateEncryptor()
+            Dim bytes As Byte() = Encoding.ASCII.GetBytes(string_1)
+            result = Convert.ToBase64String(cryptoTransform.TransformFinalBlock(bytes, 0, bytes.Length))
+        Else
+            result = ""
+        End If
+        Return result
+    End Function
+
+    Public Function Desencriptar(string_1 As String) As String
+        Dim result As String
+        If Operators.CompareString(Strings.Trim(string_1), "", False) <> 0 Then
+            tripleDESCryptoServiceProvider_0.Key = md5CryptoServiceProvider_0.ComputeHash(New UnicodeEncoding().GetBytes(Me.Llave))
+            tripleDESCryptoServiceProvider_0.Mode = CipherMode.ECB
+            Dim cryptoTransform As ICryptoTransform = tripleDESCryptoServiceProvider_0.CreateDecryptor()
+            Dim array As Byte() = Convert.FromBase64String(string_1)
+            result = Encoding.ASCII.GetString(cryptoTransform.TransformFinalBlock(array, 0, array.Length))
+        Else
+            result = ""
+        End If
+        Return result
+    End Function
+End Class
